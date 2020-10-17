5695474c64414017059fb4e6ddf8da770c07da42
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-10-17 10:44:07 +0300

    add modal window review and bue

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-const buttonBue = document.querySelector("#btn-bue");
+const buttonBue = document.querySelector('#btn-bue');
 buttonBue.addEventListener("click", function() {
-  const subscribeWindow = document.querySelector(".questions__subscribe");
+  const subscribeWindow = document.querySelector('.questions__subscribe');
   subscribeWindow.classList.remove('hidden')
+  subscribeWindow.classList.add('questions__subscribe-visible')
 });
 
-const buttonCancel = document.querySelector("#btn-cancel");
+const buttonCancel = document.querySelector('#btn-cancel');
 buttonCancel.addEventListener("click", function() {
-   const reviewWindow = document.querySelector(".questions__review");
+   const reviewWindow = document.querySelector('.questions__review');
    reviewWindow.classList.remove('hidden')
+   reviewWindow.classList.add('questions__review-visible')
+});
+
+const buttonBueBack = document.querySelector('.questions__subscribe-back');
+buttonBueBack.addEventListener("click", function() {
+  const subscribeWindow = document.querySelector('.questions__subscribe');
+  subscribeWindow.classList.remove('questions__subscribe-visible')
+  subscribeWindow.classList.add('hidden')
+});
+
+const buttonReviewBack = document.querySelector('.questions__review-back');
+buttonReviewBack.addEventListener("click", function() {
+  const reviewWindow = document.querySelector('.questions__review');
+  reviewWindow.classList.remove('questions__review-visible')
+  reviewWindow.classList.add('hidden')
 });
